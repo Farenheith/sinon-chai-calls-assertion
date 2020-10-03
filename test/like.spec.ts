2c2f5017db1f7652ae3add126a105f7e7653b6ff
Thiago Santos <thiago.santos@maxmilhas.com.br>
2020-10-03 00:27:37 -0300

    adding test case

--- test/like.spec.ts
+++ test/like.spec.ts
@@ -1,3 +1,4 @@
+import { match } from 'sinon';
 import { expect } from 'chai';
 describe('like', () => {
   it('should throw error on deep inequality', () => {
@@ -13,5 +14,9 @@ describe('like', () => {
 
   it('should not fail when value is deep equal', () => {
     expect([[[1]]]).to.be.like([[[1]]]);
+  });
+
+  it('should works with match.has', () => {
+    expect({ a: 1, b: 2}).to.be.like(match.has('a', 1));
   });
 });
